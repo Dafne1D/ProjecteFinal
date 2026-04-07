51b7ac091b9ec4470b29ca8c89636dca5f154e75
Dafne <dcarrillo572@boscdelacoma.cat>
2026-04-07 15:39:17 +0200

    Creació d'aplicació i DBConnection

---
+++ TaverEat/Infrastructure/Services/DBConnection.cs
@@ -0,0 +1,38 @@
+using Microsoft.Data.SqlClient;
+
+namespace SpotifyAPI.Services
+{
+    public class SpotifyDBConnection
+    {
+        private readonly string _connectionString;
+        public SqlConnection? sqlConnection;
+        public SpotifyDBConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection!;
+        }
+
+        public bool Open()
+        {
+            sqlConnection = new SqlConnection(_connectionString);
+
+            try
+            {
+                sqlConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public void Close()
+        {
+            sqlConnection?.Close();
+        }
+    }
+}
