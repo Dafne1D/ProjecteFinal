66d21ca365dc60205095cc6e27f6603964675918
Dafne <dcarrillo572@boscdelacoma.cat>
2026-04-29 16:09:12 +0200

    UPDATE: producte domain entity to example from github

--- Backend/TaverEat/Domain/Entities/producte.cs
+++ Backend/TaverEat/Domain/Entities/producte.cs
@@ -6,4 +6,13 @@ public class Product
     public required string Descripcio { get; set; }
     public decimal Preu { get; set; }
     public required string Categoria_nom { get; set; }
+
+    public Product(Guid id, string nom, string descripcio, decimal preu, string categoria_nom)
+    {
+        Id = id;
+        Nom = nom;
+        Descripcio = descripcio;
+        Preu = preu;
+        Categoria_nom = categoria_nom;
+    }
 }
