4943d0a265f1eb9f806588741a06f3416173a913
Dafne <dcarrillo572@boscdelacoma.cat>
2026-04-30 15:35:24 +0200

    UPDATE: comandaVendaLinea Domain entity

--- Backend/TaverEat/Domain/Entities/comandaVendaLinea.cs
+++ Backend/TaverEat/Domain/Entities/comandaVendaLinea.cs
@@ -2,6 +2,16 @@ namespace Domain.Entities;
 
 public class ComandaVendaLinea
 {
-    public required Product Product { get; set; }
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public Guid ComandaId { get; set; }
     public int Quantitat { get; set; }
+
+    public ComandaVendaLinea(Guid id, Guid productId, Guid comandaId, int quantitat)
+    {
+        Id = id;
+        ProductId = productId;
+        ComandaId = comandaId;
+        Quantitat = quantitat;
+    }
 }
