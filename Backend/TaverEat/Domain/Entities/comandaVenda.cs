cc6ca5cf0ed0d35f3b68154a59da933c6e154b01
Dafne <dcarrillo572@boscdelacoma.cat>
2026-04-30 15:28:25 +0200

    UPDATE: comandaVenda Domain entity

--- Backend/TaverEat/Domain/Entities/comandaVenda.cs
+++ Backend/TaverEat/Domain/Entities/comandaVenda.cs
@@ -1,9 +1,20 @@
 namespace Domain.Entities;
 public class ComandaVenda
 {
+    public Guid Id { get; set; }
+    public Guid ClientId { get; set; }
+    public Guid EntregaDir { get; set; }
     public DateTime Data { get; set; }
     public string Estat { get; set; }
-    public string entrega_dir { get; set; } 
-    public Client Client { get; set; }
-    public List<ComandaVendaLinea> Linies { get; set; }
+
+    public ComandaVenda(Guid id, Guid clientId, Guid entregaDir, DateTime data, string estat )
+    {
+        Id = id;
+        ClientId = clientId;
+        EntregaDir = entregaDir;
+        Data = data;
+        Estat = estat;
+    }
+
 }
+
