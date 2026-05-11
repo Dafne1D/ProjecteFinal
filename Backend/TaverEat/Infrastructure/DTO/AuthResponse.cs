d6632fb209a01bb8e9e24aab43a962be6530d27c
Dafne <dcarrillo572@boscdelacoma.cat>
2026-05-11 17:24:30 +0200

    CREATE: loginRequest.cs i authResponse per Jwt (NO funcional)

---
+++ Backend/TaverEat/Infrastructure/DTO/AuthResponse.cs
@@ -0,0 +1,3 @@
+namespace Infrastructure.DTO;
+
+public record AuthResponse(string Token, string Email);
---
+++ Backend/TaverEat/Infrastructure/DTO/LoginRequest.cs
@@ -0,0 +1,3 @@
+namespace Infrastructure.DTO;
+
+public record LoginRequest(string Email, string Contrasenya);
