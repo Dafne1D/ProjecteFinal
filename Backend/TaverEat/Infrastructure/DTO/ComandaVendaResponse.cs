97c478b998242816478a84ab9ffac60d0c248ef7
Dafne <dcarrillo572@boscdelacoma.cat>
2026-05-15 17:25:57 +0200

    UDPATE: response comanda

--- Backend/TaverEat/Infrastructure/DTO/ComandaVendaResponse.cs
+++ Backend/TaverEat/Infrastructure/DTO/ComandaVendaResponse.cs
@@ -1,11 +1,8 @@
 using Domain.Entities;
 
 namespace Infrastructure.DTO;
-
-public record ComandaVendaResponse(Guid Id, Guid ClientId, Guid EntregaDir, DateTime Data, string Estat) 
+public record ComandaVendaResponse(Guid Id, Guid ClientId, Guid EntregaDir, DateTime Data, string Estat)
 {
-    public static ComandaVendaResponse FromComandaVenda(ComandaVenda comanda)  
-    {
-        return new ComandaVendaResponse(comanda.Id, comanda.ClientId, comanda.EntregaDir, comanda.Data, comanda.Estat);
-    }
+    public static ComandaVendaResponse FromDomain(ComandaVenda comanda)
+        => new(comanda.Id, comanda.ClientId, comanda.EntregaDir, comanda.Data, comanda.Estat);
 }
