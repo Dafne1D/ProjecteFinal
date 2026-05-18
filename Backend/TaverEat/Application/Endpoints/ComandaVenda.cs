88242612cdefc9d32679c0fb2bc550a6120f4b84
Dafne <dcarrillo572@boscdelacoma.cat>
2026-05-18 18:25:25 +0200

    UPDATE: eliminar clientId en request

--- Backend/TaverEat/Application/Endpoints/ComandaVenda.cs
+++ Backend/TaverEat/Application/Endpoints/ComandaVenda.cs
@@ -10,24 +10,33 @@ public static class ComandaVendaEndpoint
 {
     public static void MapComandaVendaEndpoints(this WebApplication app)
     {
-        app.MapPost("/cart/add", (
-            AfegirProducteRequest request,
-            [FromServices] IComandaVendaRepository repo
-        ) =>
+        // ADD PRODUCT
+        app.MapPost("/cart/add", (HttpContext http,AfegirProducteRequest request, [FromServices] IComandaVendaRepository repo, [FromServices] IClientRepository clientRepo, [FromServices] JwtService jwt) =>
         {
-            // buscar comanda activa
-            var comanda = repo.GetComandaActivaByClient(request.ClientId);
+            // token
+            var token = http.Request.Headers.Authorization
+                .ToString()
+                .Replace("Bearer ", "");
 
-            // si no elineaiste -> crear
+            // email desde jwt
+            var email = jwt.ValidateAndGetEmail(token);
+
+            // client logged
+            var client = clientRepo.GetByEmail(email);
+
+            // buscar carrito activo
+            var comanda = repo.GetComandaActivaByClient(client.Id);
+
+            // si no existe -> crear
             if (comanda is null)
             {
-                comanda = repo.CreateComanda(request.ClientId);
+                comanda = repo.CreateComanda(client.Id);
             }
 
-            // buscar si ya elineaiste linea
+            // buscar linea existente
             var linea = repo.GetLinea(comanda.Id, request.ProducteId);
 
-            // si elineaiste -> sumar cantidad
+            // sumar cantidad
             if (linea is not null)
             {
                 linea.Quantitat += 1;
@@ -47,32 +56,59 @@ public static class ComandaVendaEndpoint
             return Results.Ok();
         });
 
-        // GET carrito
-        app.MapGet("/comandaVenda/{clientId}/lineas", (Guid clientId, IComandaVendaRepository repo) =>
+        // GET CART
+        app.MapGet("/cart", (
+            HttpContext http,
+            [FromServices] IComandaVendaRepository repo,
+            [FromServices] IClientRepository clientRepo,
+            [FromServices] JwtService jwt
+        ) =>
         {
-            var comanda = repo.GetComandaActivaByClient(clientId);
+            // token
+            var token = http.Request.Headers.Authorization
+                .ToString()
+                .Replace("Bearer ", "");
 
+            // email
+            var email = jwt.ValidateAndGetEmail(token);
+
+            // user
+            var client = clientRepo.GetByEmail(email);
+
+            // carrito activo
+            var comanda = repo.GetComandaActivaByClient(client.Id);
+
+            // carrito vacío
             if (comanda is null)
             {
                 return Results.Ok(
-                    new ComandaDetallResponse(Guid.Empty, new List<ComandaLineaResponse>(), 0)
+                    new ComandaDetallResponse(
+                        Guid.Empty,
+                        new List<ComandaLineaResponse>(),
+                        0
+                    )
                 );
             }
 
+            // lineas
             var lineas = repo.GetLineasWithProducte(comanda.Id);
 
-            var productes = lineas.Select(linea =>
+            // productos
+            var productes = lineas.Select(x =>
                 new ComandaLineaResponse(
-                    linea.producte.Id,
-                    linea.producte.Nom,
-                    linea.producte.Preu,
-                    linea.linea.Quantitat
+                    x.producte.Id,
+                    x.producte.Nom,
+                    x.producte.Preu,
+                    x.linea.Quantitat
                 )
             ).ToList();
+
+            // total
             decimal total = lineas.Sum(x =>
                 x.producte.Preu * x.linea.Quantitat
             );
 
+            // response
             var response = new ComandaDetallResponse(
                 comanda.Id,
                 productes,
